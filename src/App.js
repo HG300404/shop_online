576a0eb26e6a93869cfbb24d8b595285d516ef59
HG300404 <nglienhg000@gmail.com>
2023-09-29 17:06:13 +0700

    finish part 3

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { routes } from "./routes";
-import { HeaderComponent } from "./components/HeaderComponent";
-import { useSelector, useDispatch } from "react-redux";
-import { decrement, increment } from "./redux/slides/counterSlide";
-import styled from "styled-components";
+import DefaultComponent from "./components/DefaultComponent/DefaultComponent";
+// import { useSelector, useDispatch } from "react-redux";
+// import { decrement, increment } from "./redux/slides/counterSlide";
+// import styled from "styled-components";
 function App() {
   return (
     <div>
-      <HeaderComponent />
       <Router>
         <Routes>
           {routes.map((route) => {
             const Page = route.page;
-            return <Route path={route.path} element={<Page />} />;
+            const Layout = route.isShowHeader ? DefaultComponent : Fragment;
+            return (
+              <Route
+                key={route.path}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            );
           })}
         </Routes>
       </Router>
